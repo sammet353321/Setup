f831aa9174f67fdaeb54a9e0d53e92e3791f44c1
Sametcelen35 <samet_celen_35@hotmail.com>
2026-02-05 15:55:41 +0300

    Fix openExternal bug and bump to 1.0.2

--- preload.js
+++ preload.js
@@ -1,9 +1,9 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer, shell } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
   getUrl: () => ipcRenderer.invoke('get-url'),
   setUrl: (url) => ipcRenderer.invoke('set-url', url),
   close: () => ipcRenderer.send('app-close'),
   minimize: () => ipcRenderer.send('app-minimize'),
-  openExternal: (url) => require('electron').shell.openExternal(url)
+  openExternal: (url) => shell.openExternal(url)
 });
